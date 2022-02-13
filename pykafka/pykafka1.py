47fe0ade3347e5554a3fd267df3ad81611df8eee
ayushman <unixparadoxfb@gmail.com>
2022-02-13 23:15:06 +0545

    new

--- pykafka/pykafka1.py
+++ pykafka/pykafka1.py
@@ -7,4 +7,9 @@ client = KafkaClient("localhost: 9092")
 #calling a topic
 topic = client.topics["new-topic"]
 
+
+#creating a new topic
+test_topic = client.create_topic('test_topic', partitions = 2, replicas=3)
+
+
 print(topic)
