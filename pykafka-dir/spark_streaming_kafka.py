0d809373f3dc753fbdb4b1d84808780e843800ed
ayushman <unixparadoxfb@gmail.com>
2022-02-28 22:08:31 +0545

    console output spark-kafka

--- pykafka-dir/spark_streaming_kafka.py
+++ pykafka-dir/spark_streaming_kafka.py
@@ -1,8 +1,9 @@
+from typing import final
 import pyspark
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 from pyspark import SparkContext, SparkConf
-
+import time
 
 spark = SparkSession.builder.appName("Spark-kafka").getOrCreate()
 
@@ -17,9 +18,21 @@ split_col = split(query_df['value'], ',')
 query_df = query_df.withColumn('Id', split_col.__getitem__(0))
 query_df = query_df.withColumn('name', split_col.__getitem__(1))
 query_df = query_df.withColumn('email', split_col.__getitem__(2))
+query_df = query_df.withColumn('age', split_col.__getitem__(3))
+query_df = query_df.withColumn('event-type', split_col.__getitem__(4))
+query_df = query_df.withColumn('coordinate', split_col.__getitem__(5))
+query_df = query_df.withColumn('timestamp', split_col.__getitem__(6))
 
-print("Printing Schema of query_df: ")
-query_df.printSchema()
+final_op_stream_df = query_df.selectExpr("Id", "name", "coordinate", "timestamp")
+write_stream_df_op_query = final_op_stream_df.writeStream.format("console").trigger(processingTime='1 seconds').start()
+time.sleep(10)
+write_stream_df_op_query.stop()
+
+#print("Printing Schema of query_df: ")
+#query_df.printSchema()
+print("output stream................")
+#write_stream_df_op_query.awaitTermination()
+
 
  
 
