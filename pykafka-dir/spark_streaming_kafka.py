64653693ef97a70d3985a7161e5591db8875c5e7
ayushman <unixparadoxfb@gmail.com>
2022-03-04 16:51:14 +0545

    kafka-spark-rdbms

--- pykafka-dir/spark_streaming_kafka.py
+++ pykafka-dir/spark_streaming_kafka.py
@@ -1,3 +1,4 @@
+from pyspark import SparkConf, SparkContext
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 import time
@@ -8,7 +9,7 @@ CONS_KAFKA_SERVER = "localhost:9092"
 
 #starting a spark session to work with
 spark = SparkSession.builder.appName("Spark-kafka").getOrCreate()
-
+spark.sparkContext.setLogLevel("ERROR")
 
 #creating streaming dataframe
 streaming_df = spark.readStream.format("kafka").option("kafka.bootstrap.servers", CONS_KAFKA_SERVER)\
@@ -25,13 +26,14 @@ streaming_df = streaming_df.withColumn('Id', split_col.__getitem__(0))
 streaming_df = streaming_df.withColumn('name', split_col.__getitem__(1))
 streaming_df = streaming_df.withColumn('email', split_col.__getitem__(2))
 streaming_df = streaming_df.withColumn('age', split_col.__getitem__(3))
-streaming_df = streaming_df.withColumn('event-type', split_col.__getitem__(4))
-streaming_df = streaming_df.withColumn('coordinate', split_col.__getitem__(5))
-streaming_df = streaming_df.withColumn('timestamp', split_col.__getitem__(6))
+streaming_df = streaming_df.withColumn('event', split_col.__getitem__(4))
+streaming_df = streaming_df.withColumn('lat', split_col.__getitem__(5))
+streaming_df = streaming_df.withColumn('long', split_col.__getitem__(6))
+streaming_df = streaming_df.withColumn('timestamp', split_col.__getitem__(7))
 
 
 #removing the key from the data values in each column and only keeping the corresponding values
-col_array = ['Id', 'name', 'email', 'age', 'event-type', 'coordinate', 'timestamp']
+col_array = ['Id', 'name', 'email', 'age', 'event', 'lat', 'timestamp']
 df_temp = streaming_df
 for col in col_array:
     split_temp = split(df_temp[col], ":")
@@ -39,10 +41,10 @@ for col in col_array:
 
 
 #selecting the columns to stream into console using columned-query_df
-final_op_stream_df = df_temp.selectExpr("Id", "age", "coordinate", "timestamp")
+final_op_stream_df = df_temp.selectExpr("Id", "lat", "long", "timestamp")
 #streaming the data into console 
 final_op_stream_df = final_op_stream_df.writeStream.format("console").trigger(processingTime='1 seconds').start()
-time.sleep(10)
+time.sleep(2)
 #stopping the streaming data
 final_op_stream_df.stop()
 
@@ -51,22 +53,19 @@ final_op_stream_df.stop()
 
 
 
-
-
-
-
-
 #writing the streaming dataframe into mysql rdbms
-#selected_streaming_df = streaming_df.select("ID", "event-type", "coordinate", "timestamp")
+selected_streaming_df = df_temp.select("Id", "event", "lat", 'long', "timestamp")
 
-#db_properties = {'user': 'root', 'password': ''}
 
-#def for_each_batch(df, id):
-#    df.write.option("driver", "com.mysql.jdbc.Driver").mode("append").jdbc(url='jdbc:mysql://localhost:3306/test', table='ds', properties=db_properties)
-#    pass
+db_properties = {'user': 'root', 'password': ''}
+def for_each_batch(df, id):
+    df.write.option("driver", "com.mysql.jdbc.Driver").jdbc(url='jdbc:mysql://localhost:3306/test', table='demand_supply', properties=db_properties)
+    pass
 
-#query = selected_streaming_df.writeStream.foreachBatch(for_each_batch).start()
-#query.awaitTermination()
+query = selected_streaming_df.writeStream.foreachBatch(for_each_batch).start()
+print("writing to database..............")
+time.sleep(10)
+query.stop()
 
 
 
