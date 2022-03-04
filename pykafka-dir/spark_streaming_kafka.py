eea63e1169e50e67d5cb9951d238137c31ff1f94
ayushman <unixparadoxfb@gmail.com>
2022-03-04 14:29:02 +0545

    mysql working connector

--- pykafka-dir/spark_streaming_kafka.py
+++ pykafka-dir/spark_streaming_kafka.py
@@ -29,12 +29,45 @@ streaming_df = streaming_df.withColumn('event-type', split_col.__getitem__(4))
 streaming_df = streaming_df.withColumn('coordinate', split_col.__getitem__(5))
 streaming_df = streaming_df.withColumn('timestamp', split_col.__getitem__(6))
 
+
+#removing the key from the data values in each column and only keeping the corresponding values
+col_array = ['Id', 'name', 'email', 'age', 'event-type', 'coordinate', 'timestamp']
+df_temp = streaming_df
+for col in col_array:
+    split_temp = split(df_temp[col], ":")
+    df_temp = df_temp.withColumn(col, split_temp.__getitem__(1))
+
+
 #selecting the columns to stream into console using columned-query_df
-final_op_stream_df = streaming_df.selectExpr("Id", "age", "coordinate", "timestamp")
+final_op_stream_df = df_temp.selectExpr("Id", "age", "coordinate", "timestamp")
 #streaming the data into console 
 final_op_stream_df = final_op_stream_df.writeStream.format("console").trigger(processingTime='1 seconds').start()
 time.sleep(10)
 #stopping the streaming data
 final_op_stream_df.stop()
+
+
+
+
+
+
+
+
+
+
+
+#writing the streaming dataframe into mysql rdbms
+#selected_streaming_df = streaming_df.select("ID", "event-type", "coordinate", "timestamp")
+
+#db_properties = {'user': 'root', 'password': ''}
+
+#def for_each_batch(df, id):
+#    df.write.option("driver", "com.mysql.jdbc.Driver").mode("append").jdbc(url='jdbc:mysql://localhost:3306/test', table='ds', properties=db_properties)
+#    pass
+
+#query = selected_streaming_df.writeStream.foreachBatch(for_each_batch).start()
+#query.awaitTermination()
+
+
 
     
--- pykafka-dir/ss-try.py
+++ pykafka-dir/ss-try.py
@@ -1,8 +1,12 @@
 from pyspark.sql.functions import *
 from pyspark.sql import SparkSession
 import time
+import mysql.connector
+
 
 spark = SparkSession.builder.appName('test-session').getOrCreate()
+
+spark.sparkContext.setLogLevel("ERROR")
 
 df_read_stream = spark.readStream.format("kafka").option("kafka.bootstrap.servers", "localhost:9092")\
                     .option("subscribe", "test-demand2").option("startingOffsets", "earliest").load()
@@ -21,17 +25,41 @@ df_read_stream = df_read_stream.withColumn('timestamp', split_col.__getitem__(6)
 
 
 col_array = ['ID', 'name', 'email', 'age', 'event', 'coordinate', 'timestamp']
+df_temp = df_read_stream
+
 for col_name in col_array:
-    df_temp = df_read_stream.select(col_name)
     split_temp = split(df_temp[col_name], ":")
     df_temp = df_temp.withColumn(col_name, split_temp.__getitem__(1))
     
 
 #df_write_stream = df_read_stream.selectExpr('ID', 'name', 'email', "event", "coordinate", "timestamp")
 
-df_write_stream = df_temp.writeStream.format("console").trigger(processingTime='1 second').start()
-time.sleep(10)
+df_temp = df_temp.selectExpr(col_array)
+#df_write_stream = df_temp.writeStream.format("console").trigger(processingTime='1 second').start()
+time.sleep(2)
 
-df_write_stream.stop()
+#df_write_stream.stop()
+
+
+#establish mysql connection
+conn = mysql.connector.connect(user='root', database='test', password='', host='localhost', port=3306)
+if conn:
+    print("connection established........")
+else:
+    print("error in establishing connection")
+
+
+df_rdbms = df_temp.select("ID", "name", "coordinate")
+
+
+#writing the selected streaming dataframe into mysql rdbms
+db_target_properties = {'user': 'root', 'password': ''}
+
+def for_each_batch_function(df, epoch_id):
+    df.write.option("driver", "com.mysql.jdbc.Driver").mode("append").jdbc(url='jdbc:mysql://localhost:3306/test', table='test_data', properties=db_target_properties)
+    pass
+
+query = df_rdbms.writeStream.foreachBatch(for_each_batch_function).start()
+query.awaitTermination()
 
 
